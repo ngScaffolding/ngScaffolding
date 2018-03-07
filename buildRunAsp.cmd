71e11e6bb4e0b0a0616a028c8d7a610cbb553ab5
Darren Baines <dbaines1@hotmail.com>
2018-03-07 18:04:40 +0000

    build Changes

--- buildRunAsp.cmd
+++ buildRunAsp.cmd
@@ -9,4 +9,8 @@ cd ngScaffolding-oauth\bin\Debug\netcoreapp2.0\publish
 start dotnet ngScaffolding-oauth.dll
 
 
+cd ..\..\..\..\..
+cd ngScaffolding-demoApp\bin\Debug\netcoreapp2.0\publish
+start dotnet ngScaffolding-demoApp.dll
+
 cd ..\..\..\..\..\..\ngscaffolding
