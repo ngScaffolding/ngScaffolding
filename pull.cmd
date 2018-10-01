d8a85bb815443282b63920077b8e5c9eedad2162
Darren Baines <dbaines1@hotmail.com>
2018-10-01 10:11:31 +0100

    Repository renaming

--- pull.cmd
+++ pull.cmd
@@ -1,15 +1,5 @@
-git clone https://github.com/ngScaffolding/aspnet.git ..\aspnet
-git clone https://github.com/ngScaffolding/core.git ..\core
-git clone https://github.com/ngScaffolding/datagrid.git ..\datagrid
-git clone https://github.com/ngScaffolding/demoapp.git ..\demoapp
-git clone https://github.com/ngScaffolding/host.git ..\host
-git clone https://github.com/ngScaffolding/inputbuilder.git ..\inputbuilder
-git clone https://github.com/ngScaffolding/nodeapi.git ..\nodeapi
-git clone https://github.com/ngScaffolding/models.git ..\models
+git clone https://github.com/ngScaffolding/ngscaffolding-host.git ..\host
+git clone https://github.com/ngScaffolding/ngscaffolding-nodeapi.git ..\nodeapi
+git clone https://github.com/ngScaffolding/ngscaffolding-models.git ..\models
 
-git clone https://github.com/ngScaffolding/nodeOAuth.git ..\nodeOAuth
-
-git clone https://github.com/ngScaffolding/data_MySQL.git ..\data_MySQL
-git clone https://github.com/ngScaffolding/data_API.git ..\data_API
-git clone https://github.com/ngScaffolding/data_MSSQL.git ..\data_MSSQL
-git clone https://github.com/ngScaffolding/data_mongoDB.git ..\data_mongoDB
+git clone https://github.com/ngScaffolding/ngscaffolding-nodeOAuth.git ..\nodeOAuth
