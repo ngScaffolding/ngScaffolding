3154ce812c3c8331b382e8205fe6ecd98488f28b
Darren Baines <dbaines1@hotmail.com>
2018-10-29 20:27:35 +0000

    Mobile addition

--- pull.cmd
+++ pull.cmd
@@ -3,3 +3,6 @@ git clone https://github.com/ngScaffolding/ngscaffolding-nodeapi.git ..\nodeapi
 git clone https://github.com/ngScaffolding/ngscaffolding-models.git ..\models
 
 git clone https://github.com/ngScaffolding/ngscaffolding-nodeOAuth.git ..\nodeOAuth
+
+git clone https://github.com/ngScaffolding/ngscaffolding-mobile.git ..\mobile
+
